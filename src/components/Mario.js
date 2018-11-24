b845c5c4292dc14b42c44ee77970834724d65d27
Nicholas Crispino <crispinonicky@gmail.com>
2018-11-23 22:28:15 -0500

    completed the imports in the mario component

--- src/components/Mario.js
+++ src/components/Mario.js
@@ -6,6 +6,26 @@ import jab1 from "../images/mario/jab1.gif";
 import jab2 from "../images/mario/jab2.gif";
 import jab3 from "../images/mario/jab3.gif";
 import ftilt from "../images/mario/ftilt.gif";
+import dtilt from "../images/mario/dtilt.gif";
+import uptilt from "../images/mario/uptilt.gif";
+import dashattack from "../images/mario/dashattack.gif";
+import fsmash from "../images/mario/fsmash.gif";
+import dsmash from "../images/mario/dsmash.gif";
+import upsmash from "../images/mario/upsmash.gif";
+import nair from "../images/mario/nair.gif";
+import fair from "../images/mario/fair.gif";
+import dair from "../images/mario/dair.gif";
+import upair from "../images/mario/upair.gif";
+import bair from "../images/mario/bair.gif";
+import grab from "../images/mario/grab.gif";
+import dashgrab from "../images/mario/dashgrab.gif";
+
+// import neutralb from "../images/mario/neutralb.gif";
+// need to find a working gif
+
+import sideb from "../images/mario/sideb.gif";
+import upb from "../images/mario/upb.gif";
+import downb from "../images/mario/downb.gif";
 
 class Mario extends Component {
   render() {
@@ -13,7 +33,7 @@ class Mario extends Component {
       <div className="Mario">
         <h1>Mario</h1>
         <br />
-        
+
         <div className="card-row">
           <div className="move-card">
             <h3>Jab 1</h3>
