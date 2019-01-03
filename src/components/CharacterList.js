c2e11e34eddb1e907d211d8ab1d715c000c576dd
Nicholas Crispino <crispinonicky@gmail.com>
2019-01-02 23:15:47 -0500

    added a link for the luigi component on the app js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Mario from './Mario';
+import Luigi from './Luigi';
 import { Switch, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
@@ -8,13 +9,28 @@ class CharacterList extends Component {
     return (
       <div>
 
-
+<ul>
+  <li>
 <Link to ='/mario'>Mario</Link>
+</li>
 
 <Switch>
   <Route exact path = '/mario' component={Mario}/>
 </Switch>
 
+  <li>
+
+<Link to ='/luigi'>Luigi</Link>
+
+</li>
+
+
+<Switch>
+  <Route exact path = '/luigi' component={Luigi}/>
+</Switch>
+
+
+</ul>
       </div>
     );
   }
