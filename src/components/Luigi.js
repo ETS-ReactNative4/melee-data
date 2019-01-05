5a669ebde062f6e9c810bd0f3ff77c0ef941a973
Nicholas Crispino <crispinonicky@gmail.com>
2019-01-04 23:34:04 -0500

    updated visuals and imports for luigi component

--- src/components/Luigi.js
+++ src/components/Luigi.js
@@ -2,33 +2,33 @@ import React, { Component } from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 import Gif from "./Gif";
-import jab1 from "../images/mario/jab1.gif";
-import jab2 from "../images/mario/jab2.gif";
-import jab3 from "../images/mario/jab3.gif";
-import ftilt from "../images/mario/ftilt.gif";
-import dtilt from "../images/mario/dtilt.gif";
-import uptilt from "../images/mario/uptilt.gif";
-import dashattack from "../images/mario/dashattack.gif";
-import fsmash from "../images/mario/fsmash.gif";
-import dsmash from "../images/mario/dsmash.gif";
-import upsmash from "../images/mario/upsmash.gif";
-import nair from "../images/mario/nair.gif";
-import fair from "../images/mario/fair.gif";
-import dair from "../images/mario/dair.gif";
-import upair from "../images/mario/upair.gif";
-import bair from "../images/mario/bair.gif";
-import grab from "../images/mario/grab.gif";
-import dashgrab from "../images/mario/dashgrab.gif";
-import neutralb from "../images/mario/neutralb.gif";
-import sideb from "../images/mario/sideb.gif";
-import upb from "../images/mario/upb.gif";
-import downb from "../images/mario/downb.gif";
+import jab1 from "../images/luigi/jab1.gif";
+import jab2 from "../images/luigi/jab2.gif";
+import jab3 from "../images/luigi/jab3.gif";
+import ftilt from "../images/luigi/ftilt.gif";
+import dtilt from "../images/luigi/dtilt.gif";
+import uptilt from "../images/luigi/uptilt.gif";
+import dashattack from "../images/luigi/dashattack.gif";
+import fsmash from "../images/luigi/fsmash.gif";
+import dsmash from "../images/luigi/dsmash.gif";
+import upsmash from "../images/luigi/upsmash.gif";
+import nair from "../images/luigi/nair.gif";
+import fair from "../images/luigi/fair.gif";
+import dair from "../images/luigi/dair.gif";
+import upair from "../images/luigi/upair.gif";
+import bair from "../images/luigi/bair.gif";
+import grab from "../images/luigi/grab.gif";
+import dashgrab from "../images/luigi/dashgrab.gif";
+import neutralb from "../images/luigi/neutralb.gif";
+import sideb from "../images/luigi/sideb.gif";
+import upb from "../images/luigi/upb.gif";
+import downb from "../images/luigi/downb.gif";
 
-class Mario extends Component {
+class Luigi extends Component {
   render() {
     return (
       <div>
-        <h1>Mario</h1>
+        <h1>Luigi</h1>
         <div className="all-moves">
           <div className="move-card">
             <h3>Jab 1</h3>
@@ -225,4 +225,4 @@ class Mario extends Component {
   }
 }
 
-export default Mario;
+export default Luigi;
